d6ace1beb0c23664215f05dd0fb8782c0044e45b
David Stein <dstein@flatironinstitute.org>
2018-09-11 17:01:04 -0400

    stuff

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ f_module = Extension( 'fortran_laplace_kernel',
                     )
 
 c_module = Extension( 'c_laplace_kernel',
-                        ['src/c_laplace_kernel.cpp','src/Timer.hpp','src/AlignedMemory.hpp'],
+                        ['src/c_laplace_kernel.cpp'],#,'src/Timer.hpp','src/AlignedMemory.hpp'],
                         extra_compile_args=['-std=c++11', '-O3', '-fopenmp', '-march=native'],
                         # extra_compile_args=['-std=c++11', '-qopenmp', '-O3', '-xHost'],
                         libraries=['gomp'],
@@ -37,7 +37,7 @@ setup(
         'License :: Apache 2',
         'Programming Language :: Python :: 2',
     ],
-    ext_modules =   [f_module, c_module],
+    ext_modules=[f_module, c_module],
     packages=find_packages(),
     install_requires=[],
 )
