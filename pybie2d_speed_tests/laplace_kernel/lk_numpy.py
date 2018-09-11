aaba1f5e5df2182d2deaea8b963e2076f4956536
David Stein <dstein@flatironinstitute.org>
2018-09-11 15:35:41 -0400

    stuff

--- pybie2d_speed_tests/laplace_kernel/lk_numpy.py
+++ pybie2d_speed_tests/laplace_kernel/lk_numpy.py
@@ -32,6 +32,5 @@ def lk_numpy_loop(source, target, density):
     for j in range(M):
         dx = source[0] - target[0,j]
         dy = source[1] - target[1,j]
-        d = np.log(dx**2 + dy**2)
-        out += d.dot(sw)
+        out += np.log(dx**2 + dy**2)*sw
     return out
